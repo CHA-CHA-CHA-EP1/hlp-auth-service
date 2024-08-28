1077cc51832b190b5103fdafb73b81ad53e57c6b
Suthiphong Thaisuriya <suthiphong.t@arise.tech>
2024-08-29 00:02:15 +0700

    modify health-check path

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 use actix_cors::Cors;
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder};
 
 use auth_service::controllers;
-
-#[get("/health-check")]
-async fn index() -> impl Responder {
-    "Hello world!"
-}
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -19,8 +13,10 @@ async fn main() -> std::io::Result<()> {
         let cors = Cors::default();
         App::new()
             .wrap(cors)
+            .route("/health-check", 
+                web::get().to(controllers::health_check::health_check),
+            )
             .configure(controllers::auth_controller::config)
-            .service(index)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
